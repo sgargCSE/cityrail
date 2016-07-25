19c7efa8d8cdd23130a53abed0a883ffd8221756
Shivam Garg <sgarg@cse.unsw.edu.au>
2016-07-25 19:14:19 +1000

    cleanup, and best 10

--- test.php
+++ test.php
@@ -22,27 +22,33 @@ function getTrains($xml, $start, $end, $tnow) {
    $sXml = getXmlRow($xml, $start);
    $eXml = getXmlRow($xml, $end);
 
+   //TODO: Any times before 4pm modify the time to add 12hours to it.
    foreach($sXml as $ind=>$time) {
-      $toAdd = array("s"=>strtotime($time), "e"=> strtotime($eXml[$ind]));
+      $toAdd = array("s"=>strtotime($time), "e"=> strtotime($eXml[$ind]), "raw"=>($time . "-" . $eXml[$ind]));
       if($toAdd['s'] && $toAdd['e'] && $tnow <= $toAdd['s']){
          $toAdd['sStr'] = date("H:i", $toAdd['s']);
          $toAdd['eStr'] = date("H:i", $toAdd['e']);
          $arr[] = $toAdd;
          //print $toAdd['sStr'] . " - " . $toAdd['eStr'] . "\n";
+      } else {
+         //print $toAdd["raw"] . "\n";
       }
    }
 
    return $arr;
 }
-/*
+
 function sortByQuickest(&$toSort) {
    $tempArray = array();
-   foreach(
+   foreach($toSort as $k=>$v) {
+      $tempArray[$k] = $v['e'];
+   }
+   array_multisort($tempArray, SORT_ASC, $toSort);
 }
- */
 
 $_GET['start'] = 'Hurstville'; 
 $_GET['end'] = 'Martin Place';
+$_GET['time'] = '9:00';
 
 foreach ($argv as $arg) {
    $e=explode("=",$arg);
@@ -54,19 +60,23 @@ foreach ($argv as $arg) {
 
 $str = file_get_contents('cityrail.html');
 $xml = simplexml_load_string($str);
-$tNow = strtotime('09:00'); //time()
+$tNow = strtotime($_GET['time']); //time()
 $allTrains = getTrains($xml, $_GET['start'], $_GET['end'], $tNow);
-$subset = array_slice($allTrains, 0, 5);
+
+//sortByQuickest($allTrains);
+
+$subset = array_slice($allTrains, 0, 10);
 $formatted = array();
 foreach($subset as $v) {
    $formatted[] = array('title'=>$v['sStr'], 'value'=> 'Gets there at: '.$v['eStr']); 
 }
 
-//var_dump($formatted);
+foreach($subset as $v) {
+   print $v['eStr'] . "\n";
+}
 
 $json = json_encode($formatted);
 print $json;
 
 ?>
-
 
